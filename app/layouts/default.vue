3c2767e13bb0f1795a7d12e7e97be77968f45314
Muddasir Nisar <mnisar@teresol.com>
2026-02-03 16:44:36 +0500

    Added Explore Btn Component and ClientOnly directive for LightRays.

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,25 +1,30 @@
 <template>
   <div>
-    <!-- Example: Navbar -->
+
     <Navbar />
 
     <div class="absolute inset-0 top-0 z-[-1] min-h-screen">
-      <LightRays
-        rays-origin="top-center"
-        rays-color="#00ffff"
-        :rays-speed="1.5"
-        :light-spread="0.8"
-        :ray-length="1.2"
-        :follow-mouse="true"
-        :mouse-influence="0.1"
-        :noise-amount="0.1"
-        :distortion="0.05"
-        class-name="custom-rays"
-      />
+      <ClientOnly>
+        <LightRays
+          rays-origin="top-center"
+          rays-color="#00ffff"
+          :rays-speed="1.5"
+          :light-spread="0.8"
+          :ray-length="1.2"
+          :follow-mouse="true"
+          :mouse-influence="0.1"
+          :noise-amount="0.1"
+          :distortion="0.05"
+          class-name="custom-rays"
+        />
+      </ClientOnly>
     </div>
 
     <!-- Page content -->
-    <NuxtPage />
+    <main>
+      <NuxtPage />
+    </main> 
+    
 
   </div>
 </template>
