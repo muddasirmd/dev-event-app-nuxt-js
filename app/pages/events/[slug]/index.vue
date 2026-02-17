bf5ea40992af3041a4c28743913a8f645d3d3c56
Muddasir Nisar <mnisar@teresol.com>
2026-02-17 17:45:54 +0500

    Added Event Details Page

---
+++ app/pages/events/[slug]/index.vue
@@ -0,0 +1,33 @@
+<template>
+    <section id="event">
+        <div class="header">
+            <h1>Event Description</h1>
+            <p>{{ description }}</p>
+        </div>
+
+        <div class="details">
+            <!-- Left Side - Event Content  -->
+             <div class="content">
+                <NuxtImg :src="image" alt="Event Banner" width="800" height="800" class="banner" />
+
+                <section class="flex-col-gap-2">
+                    <h2>Overview</h2>
+                    <p>{{ overview }}</p>
+                </section>
+
+                <section class="flex-col-gap-2">
+                    <h2>Event Details</h2>
+                </section>
+
+             </div>
+        </div>
+    </section>
+</template>
+
+
+<script setup lang="ts">
+const Child = {
+    props: ['title'],
+    template: `<div> {{ title }} </div>`
+}
+</script>
