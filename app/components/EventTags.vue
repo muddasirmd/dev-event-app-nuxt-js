fd48690a28d8488c67f82d04bafa4a89a5010568
Muddasir Nisar <mnisar@teresol.com>
2026-02-25 10:21:58 +0500

    Added reusable components

---
+++ app/components/EventTags.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="flex flex-row gap-1.5 flex-wrap">
+        <div v-for="tag in tags" :key="tag" class="pill">
+            {{ tag }}
+        </div>
+    </div>
+
+</template>
+
+<script setup lang="ts">
+
+interface Props{
+    tags: string[]
+}
+
+const { tags } = defineProps<Props>()
+</script>
